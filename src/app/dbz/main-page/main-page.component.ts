6cd7c8f9e4115fbbd6392cef855d435c1b8ceb1e
Carlos Raez <carlosraez@gmail.com>
2022-12-03 15:08:04 +0100

    feat list componentAngular

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  name:string;
+  power:number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
+   
+   personajes: Personaje[] = [
+    {name: 'Goku', power: 1500},
+    {name: 'Vegeta', power: 200}
+  ]
+   new:Personaje = {
+     name: '',
+     power: 0,
+   }
+
    add() {
-    
+    if (this.new.name.trim().length === 0) {
+      return
+    }
+    this.personajes.push(this.new)
+    this.new = {
+      name: '',
+      power: 0,
+    }
    }
 }
